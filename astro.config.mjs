45ca90fc49b55575e663023e0bf25364910b740f
Courtney Hutton Pospick <43227289+courtnicole@users.noreply.github.com>
2025-03-01 16:58:04 -0600

    Update to include astro content collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,36 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
+import compress from 'astro-compress'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), icon({
-    iconDir: "src/assets/icons",
-    include: {
-      teenyicons: ['vr-headset-outline'],
-      ion: ['bookmark-outline', 'information-circle-outline', 'notifications-outline', 'checkbox-outline', 'warning-outline', 'alert-circle-outline'],
-      mdi: ['email', 'linkedin']
-    }
-  }), mdx()]
-});
+  compressHTML: true,
+  site: 'https://huttonpospick.com/',
+  integrations: [
+    mdx(),
+    compress(),
+    tailwind({
+      applyBaseStyles: false
+    }),
+    icon({
+      iconDir: "src/assets/icons",
+      include: {
+        teenyicons: ['vr-headset-outline'],
+        ion: ['bookmark-outline', 'information-circle-outline', 'notifications-outline', 'checkbox-outline', 'warning-outline', 'alert-circle-outline'],
+        mdi: ['email', 'linkedin']
+      }
+    })
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          logger: {
+            warn: () => { },
+          },
+        },
+      },
+    },
+  },
+})
--- src/content/updates/update-1.md
+++
@@ -1,8 +0,0 @@
----
-title: Publication available
-pubDate: 2023-12-06
----
-
-My recent journal paper is now [available online](https://www.frontiersin.org/articles/10.3389/frvir.2023.1192757/full) as part of the Frontiers research Topic Beyond Touch: Free Hand Interaction in Virtual Environments.
-
-```
